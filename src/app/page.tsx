4e4224aec171dc28d7df3ad2008929203d02484f
Victor de Victa <victordevicta@gmail.com>
2023-11-28 10:21:54 +1100

    fix: export healthcheck function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,22 @@
 import { useEffect } from "react";
 import Button from "@/components/Button/Button";
 
-export default function App() {
-  const healthcheck = async () => {
-    try {
-      const response = await fetch("/api/healthcheck", {
-        method: "GET",
-      });
+export async function healthcheck() {
+  try {
+    const response = await fetch("/api/healthcheck", {
+      method: "GET",
+    });
 
-      if (response) {
-        const { server } = await response.json();
-        console.log("server health status:", server);
-      }
-    } catch (error) {
-      console.log(error);
+    if (response) {
+      const { server } = await response.json();
+      console.log("server health status:", server);
     }
-  };
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+export default function App() {
   useEffect(() => {
     healthcheck();
   }, []);
